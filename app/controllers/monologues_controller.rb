75a568702ef1c87b64fa65ebb10c95405340ecd7
Takuya Endou <xkinked.tailx@gmail.com>
2019-02-20 08:20:44 +0000

    new.html.erbとedit.html.erbと削除機能の追加、topページにそれらへのリンクを追加

--- app/controllers/monologues_controller.rb
+++ app/controllers/monologues_controller.rb
@@ -1,5 +1,20 @@
 class MonologuesController < ApplicationController
-    def index
-    end
     
+    def index
+        @monologues = Monologue.all
+    end
+    def edit
+    end
+    def create
+    end
+    def new
+        @monologue = Monologue.new
+    end
+    private
+    def set_monologue
+        @monologue = Monologue.find(params[:id])
+    end
+    def monologue_params
+        params.require(:monologue).permit(:content)
+    end
 end
