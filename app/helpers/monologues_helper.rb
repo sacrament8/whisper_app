802de17b795a19cac7be891831737e47a5cb598d
Takuya Endou <xkinked.tailx@gmail.com>
2019-02-20 14:43:53 +0000

    helperメソッドを2つ追記、内容はコメントアウトに

--- app/helpers/monologues_helper.rb
+++ app/helpers/monologues_helper.rb
@@ -1,2 +1,18 @@
 module MonologuesHelper
+# _form.html.erb送信先(url:)のpathをactionによって切り替える
+    def new_or_edit_path
+        if action_name == "new"
+            confirm_monologues_path
+        elsif action_name == "edit"
+            monologue_path
+        end
+    end
+# _form.html.erbのsubmitボタンの名前をactionによって切り替える
+    def new_or_edit_submit_message
+        if action_name == "new"
+            "ささやく"
+        elsif action_name == "edit"
+            "Edit"
+        end
+    end
 end
