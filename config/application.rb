9f8d6cc7a244ce9e0a323b2825d15722c1fa04ea
Takuya Endou <xkinked.tailx@gmail.com>
2019-02-20 07:31:16 +0000

    タイムゾーン変更のconfig記述をwhisper module内に修正

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ Bundler.require(*Rails.groups)
 module Whisper
   class Application < Rails::Application
     config.load_defaults 5.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
   end
 end
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
