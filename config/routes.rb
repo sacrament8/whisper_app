e4f1fd0c7e0906b4038bcca85660a6f4f9cafcb9
Takuya Endou <xkinked.tailx@gmail.com>
2019-02-21 16:13:40 +0000

    routes.rbの余計なコメントを削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,3 @@ Rails.application.routes.draw do
     post :confirm, on: :collection
   end
 end
-
-=begin
-        Prefix Verb   URI Pattern                    Controller#Action
-    monologues GET    /monologues(.:format)          monologues#index
-               POST   /monologues(.:format)          monologues#create
- new_monologue GET    /monologues/new(.:format)      monologues#new
-edit_monologue GET    /monologues/:id/edit(.:format) monologues#edit
-     monologue GET    /monologues/:id(.:format)      monologues#show
-               PATCH  /monologues/:id(.:format)      monologues#update
-               PUT    /monologues/:id(.:format)      monologues#update
-               DELETE /monologues/:id(.:format)      monologues#destroy
-=end
